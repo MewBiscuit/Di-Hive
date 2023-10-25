f0713ec1fdfc5852d2b86ed33955ace30a0497d9
MewBiscuit <i.ona@alumnos.upm.es>
2023-10-25 20:21:45 +0200

    Fixed web_provisioning

--- components/web_provisioning/web_provisioning.c
+++ components/web_provisioning/web_provisioning.c
@@ -2,6 +2,8 @@
 #include "nvs_manager.h"
 #include <string.h>
 
+#include "web_server.h"
+
 /* Wi-Fi credentials */
 static char ssid[32] = {0};
 static char password[64] = {0};
@@ -12,7 +14,45 @@ static esp_err_t root_handler(httpd_req_t *req)
                         "<label>SSID: <input type=\"text\" name=\"ssid\"></label><br>"
                         "<label>Password: <input type=\"password\" name=\"password\"></label><br>"
                         "<input type=\"submit\" value=\"Connect\"></form></body></html>";
-    httpd_resp_send(req, html, -1);
+    
+    //Get credentials when POST request is received
+    if (req->method == HTTP_POST)
+    {
+        int total_len = req->content_len;
+        int cur_len = 0;
+        int received = 0;
+        char *buf = malloc(total_len);
+        if (!buf)
+        {
+            httpd_resp_send_500(req);
+            return ESP_FAIL;
+        }
+        while (cur_len < total_len)
+        {
+            received = httpd_req_recv(req, buf + cur_len, total_len);
+            if (received <= 0)
+            {
+                httpd_resp_send_500(req);
+                free(buf);
+                return ESP_FAIL;
+            }
+            cur_len += received;
+        }
+        buf[total_len] = '\0';
+
+        //Parse SSID and password from POST request
+        char param[32];
+        if (httpd_query_key_value(buf, "ssid", param, sizeof(param)) == ESP_OK)
+        {
+            strcpy(ssid, param);
+        }
+        if (httpd_query_key_value(buf, "password", param, sizeof(param)) == ESP_OK)
+        {
+            strcpy(password, param);
+        }
+        free(buf);
+    }
+
     //Save credentials to NVS
     init_nvs();
     write_string_to_nvs("ssid", ssid);
