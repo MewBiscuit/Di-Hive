295f24a8794d2ecfa8217c31fce80ee5074a347b
MewBiscuit <i.ona@alumnos.upm.es>
2023-10-25 18:27:30 +0200

    Built blue-man

---
+++ components/blue_manager/include/blue_manager.h
@@ -0,0 +1,13 @@
+#ifndef BLUE_MANAGER_H
+#define BLUE_MANAGER_H
+
+#include "esp_bt.h"
+#include "esp_bt_main.h"
+#include "esp_gap_bt_api.h"
+#include "esp_gap_ble_api.h"
+#include "esp_gatts_api.h"
+#include "esp_gatt_common_api.h"
+
+void receive_file_over_bluetooth();
+
+#endif
