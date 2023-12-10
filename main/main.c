e8701262314a5ffd25587222d2337d3a27894645
MewBiscuit <i.ona@alumnos.upm.es>
2023-12-10 17:40:42 +0100

    attempt to fix

--- main/main.c
+++ main/main.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <time.h>
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -7,28 +6,54 @@
 #include "esp_log.h"
 #include "esp_sleep.h"
 
-#include "adc_man.h"
-#include "sensors_man.h"
 #include "nvs_man.h"
 #include "wifi_man.h"
 #include "mqtt_man.h"
+#include "adc_man.h"
+#include "sensors_man.h"
 
 
 #define TAG "MAIN"
 
-static esp_err_t groove_mic(int* noise, adc_oneshot_unit_handle_t adc_handle, adc_channel_t channel){
-    esp_err_t ret = ESP_OK;
-    ret = adc_manager_read_oneshot(adc_handle, channel, noise);
-    return ret;
-}
-
-void app_main(void)
-{
+void app_main(void) {
     //Variables
     int temp_in = 0;
     int hum_in = 0;
     int temp_out = 0;
     int noise = 0;
+    char* ssid = "";
+    char* password = "";
+    char* ssid_prov = "";
+    char* password_prov = "";
+    esp_err_t err = ESP_OK;
+
+    //Read wifi credentials from NVS
+    init_nvs();
+    read_string_from_nvs("ssid", ssid);
+    read_string_from_nvs("password", password);
+    read_string_from_nvs("ssid_prov", ssid_prov);
+    read_string_from_nvs("password_prov", password_prov);
+
+    //Wifi setup
+    wifi_init_sta();
+    err = connect_ap(ssid, password);
+    for(int i = 0; i < 3 && err != ESP_OK; i++) {
+        ESP_LOGI(TAG, "Error (%s) connecting to AP!", esp_err_to_name(err));
+        ESP_LOGI(TAG, "Error (%s) connecting to AP!", esp_err_to_name(err));
+        //Start SoftAP mode
+        wifi_release();
+        wifi_init_ap();
+        err = setup_ap(ssid_prov, password_prov, 1, 4);
+        //Provide valid credentials
+
+
+        //Re-attempt connection
+        wifi_release();
+        wifi_init_sta();
+        read_string_from_nvs("ssid", ssid);
+        read_string_from_nvs("password", password);
+        err = connect_ap(ssid, password);
+    }
 
     //Initialize ADC
     adc_oneshot_unit_handle_t adc_handle = adc_manager_init(ADC_UNIT_1);
@@ -36,10 +61,9 @@ void app_main(void)
 
 
     //Execution loop
-    while(1){
+    while(1) {
         //Read data
         vTaskDelay(1000 / portTICK_PERIOD_MS);
-        groove_mic(&noise, adc_handle, ADC_CHANNEL_6);
         printf("Noise: %ddB\n", noise);
     }
 }
