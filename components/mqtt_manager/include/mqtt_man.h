05155cdc92ba7c288de93da748978522fabe91e5
MewBiscuit <i.ona@alumnos.upm.es>
2023-12-22 23:04:26 +0100

    Cleaned up header file

--- components/mqtt_manager/include/mqtt_man.h
+++ components/mqtt_manager/include/mqtt_man.h
@@ -25,10 +25,6 @@
 
 static const char *MQTT_TAG = "MQTT_MAN";
 
-static void log_error_if_nonzero(const char *message, int error_code);
-
-static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data);
-
 /**
  * @brief Connect to MQTT broker with username and password
  * 
