afe15908d72a07cc7c77a69ec1f805aaf5ff38d4
MewBiscuit <i.ona@alumnos.upm.es>
2024-06-11 18:56:25 +0200

    HX711 is now reading, ESP32 can only power 2 load cells

--- src/main.c
+++ src/main.c
@@ -4,7 +4,6 @@
 #include "mqtt_man.h"
 #include "sensors_man.h"
 #include "ota_man.h"
-#include "sd_man.h"
 
 //Standard libraries
 #include "esp_sleep.h"
@@ -12,90 +11,32 @@
 //Logs
 #define TAG "MAIN"
 
-//Telemetry
-#define SERVER "mqtt://demo.thingsboard.io"
-#define TOKEN ""
-#define TOPIC "v1/devices/me/telemetry"
-const int port = 1883;
-
-//NVS
-char ssid_var[256] = "dummy_data";
-char password_var[250] = "dummy_data";
-
-//WiFi
-#define AP_NAME "Di-Core_Prov"
-#define AP_PWD "Provisioning123"
-bool provisioned = false, credentials = false, wifi_flag = false;
-int max_connections = 4, channel = 1;
-
-//Telemetry
-esp_mqtt_client_handle_t tb_client;
-
-//Local memory
-char *file_data = "/sdcard/data.txt", *file_credentials = "/sdcard/credentials.txt";
-bool sd_flag = 0, nvs_flag = 0;
-
 void app_main() {
-    int i;
+    float weight = 0;
+    int32_t raw_data;
     esp_err_t err = ESP_OK;
+    hx711_t sensor = {.dout = GPIO_NUM_26, .pd_sck = GPIO_NUM_27, .gain = HX711_GAIN_A_128};
 
-    for(i = 0, err = init_nvs(); i < 10 && err != ESP_OK; i++) {
-        err = init_nvs();
-    }
+    printf("Initializing sensor...\n");
+    err = HX711_init(&sensor);
 
-    //If we can't initiate NVS, restart. If already restarted, NVS is deemed unusable.
     if(err != ESP_OK) {
-        if(rtc_get_reset_reason(0) == SW_CPU_RESET) {
-            nvs_flag = 1;
-        }
-        else {
-            esp_restart();
-        }
+        printf("Couldn't initialize hx771\n");
     }
 
-    else if(read_string_from_nvs("ssid", ssid_var) == ESP_OK && read_string_from_nvs("password", password_var) == ESP_OK)
-        credentials = true;
-
-    err = init_sd();
-    if(err != ESP_OK)
-        sd_flag = 1;
-
-    else if(!credentials)
-        read_sd_creds(&credentials, ssid_var, password_var);
-
-    //WiFi initialization
-    err = wifi_init();
-    if(err != ESP_OK)
-        wifi_flag = true;
-    
-    //2 states, we have access to WiFi module or we don't
-    //If WiFi module works
-    if(!wifi_flag) {
-        if(credentials) { //We found credentials
-            err = connect_ap(ssid_var, password_var);
-            if (err != ESP_OK) {
-                setup_ap(AP_NAME, AP_PWD, &channel, &max_connections);
-                printf("Setup AP\n");
+    else{
+        printf("Done\n");
+        while(true) {
+            printf("Reading sensor...\n");
+            if(HX711_read(&sensor, &weight, &raw_data) != ESP_OK) {
+                printf("Error reading hx771\n");
             }
+
+            else {
+                printf("Raw reading: %d g\n", raw_data);
+            }
+
+            vTaskDelay(2000 / portTICK_PERIOD_MS);
         }
-
-        while(!connected) {
-            while(!provisioned) {
-                vTaskDelay(30000 / portTICK_PERIOD_MS);
-                is_provisioned(&provisioned);
-            }
-            printf("Device was provisioned\n");
-            err = connect_ap(ssid_var, password_var);
-            if (err != ESP_OK) {
-                setup_ap(AP_NAME, AP_PWD, &channel, &max_connections);
-                printf("Setup AP\n");
-            }
-        }
-
-        //Check for updates
-        printf("Checking for updates");
-        check_updates();
-        esp_ota_mark_app_valid_cancel_rollback();
-        esp_restart();
     }
 }
